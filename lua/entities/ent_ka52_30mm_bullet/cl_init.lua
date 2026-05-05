faa25e235df727ccb1b9ffe351a595183753940c
NachinBombin <solidnachox@gmail.com>
2026-05-05 00:44:43 -0400

    Fix triple projectile: remove client extrapolation, all visuals use entity pos

--- lua/entities/ent_ka52_30mm_bullet/cl_init.lua
+++ lua/entities/ent_ka52_30mm_bullet/cl_init.lua
@@ -2,31 +2,12 @@ include("shared.lua")
 
 local MAT_GLOW = Material("sprites/light_glow02_add")
 
-local _bulletData = {}
-
 -- ============================================================
--- NET
+-- NET -- consume packets only, no client-side position tracking
 -- ============================================================
-net.Receive("ka52_bullet_tracer", function()
-    local mpos     = net.ReadVector()
-    local dir      = net.ReadVector()
-    net.ReadBool()
-    local entIdx   = net.ReadUInt(16)
-
-    dir:Normalize()
-    _bulletData[entIdx] = {
-        pos = mpos,
-        dir = dir,
-        vel = 25000,
-        t0  = CurTime(),
-    }
-end)
-
-net.Receive("ka52_bullet_pos",    function() net.ReadUInt(16) net.ReadVector() net.ReadVector() end)
-net.Receive("ka52_bullet_remove", function()
-    local idx = net.ReadUInt(16)
-    _bulletData[idx] = nil
-end)
+net.Receive("ka52_bullet_tracer",  function() net.ReadVector() net.ReadVector() net.ReadBool() net.ReadUInt(16) end)
+net.Receive("ka52_bullet_pos",     function() net.ReadUInt(16) net.ReadVector() net.ReadVector() end)
+net.Receive("ka52_bullet_remove",  function() net.ReadUInt(16) end)
 
 -- ============================================================
 -- RENDER
@@ -36,19 +17,7 @@ function ENT:Draw()
 end
 
 function ENT:DrawTranslucent()
-    local idx  = self:EntIndex()
-    local data = _bulletData[idx]
-
-    local pos
-    if data then
-        local dt = CurTime() - data.t0
-        pos = data.pos
-            + data.dir * (data.vel * dt)
-            - Vector(0, 0, 0.5 * 600 * dt * dt)
-    else
-        pos = self:GetPos()
-    end
-
+    local pos = self:GetPos()
     render.SetMaterial(MAT_GLOW)
     render.DrawSprite(pos, 120, 120, Color(255, 140, 20, 180))
     render.DrawSprite(pos, 30,  30,  Color(255, 240, 180, 255))
